a73e6d2339496919f78f9e30e416be478dae8662
Muguku <hello@muguku.co.ke>
2020-11-26 03:35:49 +0300

    callback to home

--- components/landing.js
+++ components/landing.js
@@ -1,4 +1,5 @@
 import UserLogin from "../assets/undraw_businessman_97x4.svg";
+import Link from "next/link";
 
 export function Landing({ children }) {
   return (
@@ -9,16 +10,20 @@ export function Landing({ children }) {
           className="hidden xl:flex lg:flex w-full lg:w-6/12 xl:w-6/12 h-screen bg-no-repeat"
           style={{ backgroundImage: `url('${UserLogin}')` }}
         >
-          <h2 className="mx-auto mt-10 text-2xl text-blue-800 font-bold font-mono">
-            Welcome To Our Totally Real Business
-          </h2>
+          <Link href="/login">
+            <a className="mx-auto mt-10 text-2xl text-blue-800 font-bold font-mono">
+              Welcome To Our Totally Real Business
+            </a>
+          </Link>
         </div>
 
         {/* tab 2 */}
         <div className="flex flex-col w-full lg:w-6/12 xl:w-6/12 h-screen bg-no-repeat overflow-y-scroll">
-          <h2 className="flex xl:hidden lg:hidden mx-auto mt-10 text-base sm:text-3xl md:text-3xl text-blue-800 font-bold font-mono">
-            Welcome to Our Totally Real Business
-          </h2>
+          <Link href="/login">
+            <a className="flex xl:hidden lg:hidden mx-auto mt-10 text-base sm:text-3xl md:text-3xl text-blue-800 font-bold font-mono">
+              Welcome to Our Totally Real Business
+            </a>
+          </Link>
           {children}
           <footer className="flex flex-col md:flex-row lg:flex-row xl:flex-row mx-auto mb-3 text-sm lg:text-xs italic text-blue-800 font-extrabold">
             ©{new Date().getFullYear()} Go-lang, GraphQL, Next.js,
